e597e70bd18126d16d4de79b248b06445b393ca3
Joshua Suskalo <joshua@suskalo.org>
2021-06-14 23:03:14 -0500

    Add basic clj implementation

--- src/clj/ropes/core.clj
+++ src/clj/ropes/core.clj
@@ -1,1 +1,136 @@
-(ns ropes.core)
+(ns ropes.core
+  (:refer-clojure :exclude [concat split replace])
+  (:import
+   (clojure.lang
+    Counted IHashEq IMeta IObj IPersistentCollection
+    Seqable SeqIterator Sequential)
+   (java.io Writer)
+   (java.util List))
+  (:require [clojure.string :as str]))
+
+(declare rope)
+
+(deftype Rope [left right weight count data meta]
+  Counted
+  (count [_] count)
+
+  IHashEq
+  (hasheq [this]
+    (hash-ordered-coll (seq this)))
+
+  IMeta
+  (withMeta [_ meta]
+    (Rope. left right weight count data meta))
+
+  IObj
+  (meta [_] meta)
+
+  IPersistentCollection
+  (cons [this s]
+    (Rope. this (rope [s]) count (inc count) nil meta))
+  (empty [this]
+    (Rope. nil nil 0 0 nil meta))
+  (equiv [this other]
+    (cond
+      (identical? this other) true
+      (or (instance? Rope other)
+          (instance? List other)
+          (sequential? other)) (= (seq this) (seq other))
+      :else false))
+
+  Seqable
+  (seq [_]
+    (if (or (some? data)
+            (and (nil? left)
+                 (nil? right)))
+      (seq data)
+      (lazy-cat (seq left) (seq right))))
+
+  Sequential
+
+  Iterable
+  (iterator [this]
+    (SeqIterator. (seq this))))
+
+(defn rope
+  ([] (rope nil nil))
+  ([s] (rope s nil))
+  ([s m] (Rope. nil nil (count s) (count s) s m)))
+
+(defn rope?
+  [r]
+  (instance? Rope r))
+
+(defn concat
+  ([] (rope))
+  ([x]
+   (if-not (rope? x) (rope x) x))
+  ([x y]
+   (let [x (if-not (rope? x) (rope x) x)
+         y (if-not (rope? y) (rope y) y)]
+     (Rope. x y (.-count x) (+ (.-count x) (.-count y))
+            nil (merge (meta x) (meta y)))))
+  ([x y & more]
+   (reduce concat (list* x y more))))
+
+(defn split
+  [^Rope r idx]
+  {:pre [(< idx (count r))]}
+  (letfn [(s [^Rope r idx]
+            (if (some? (.-data r))
+              (cond
+                (zero? idx) [(rope) r]
+                (= idx (.-count r)) [r (rope)]
+                :else (mapv #(rope % (.-meta r)) (split-at idx (.-data r))))
+              (if (< idx (.-weight r))
+                (let [[r1 r2] (split (.-left r) idx)]
+                  [r1
+                   (Rope. r2 (.-right r) (.-count r2) (+ (.-count r2) (.-count (.-right r)))
+                          nil (.-meta r))])
+                (let [[r1 r2] (split (.-right r) (- idx (.-weight r)))]
+                  [(Rope. (.-left r) r1 (.-weight r) (+ (.-weight r) (.-count r1)) nil (.-meta r))
+                   r2]))))]
+    (s (if-not (rope? r) (rope r) r) idx)))
+
+(defn snip
+  ([^Rope r start]
+   (first (split r start)))
+  ([^Rope r start end]
+   (let [[r1 r2] (split r start)
+         [_ r2] (split r2 (- end start))]
+     (concat r1 r2))))
+
+(defn view
+  ([^Rope r start]
+   (second (split r start)))
+  ([^Rope r start end]
+   (first (split (second (split r start)) (- end start)))))
+
+(defn insert
+  [^Rope r idx s]
+  (if (< idx (count r))
+    (let [[r1 r2] (split r idx)]
+      (concat r1 (if-not (rope? s) (rope s) s) r2))
+    (concat r (if-not (rope? s) (rope s) s))))
+
+(defn replace
+  [^Rope r start end s]
+  (insert (snip r start end) start s))
+
+(defmethod print-method Rope
+  [v w]
+  (.write ^Writer w "#rope [ ")
+  (doseq [item (seq v)]
+    (print-method item w)
+    (.write ^Writer w " "))
+  (.write ^Writer w "]"))
+
+(defmethod print-dup Rope
+  [v w]
+  (.write ^Writer w "#=(ropes.core/rope [ ")
+  (doseq [item (.-data ^Rope v)]
+    (print-dup item w)
+    (.write ^Writer w " "))
+  (.write ^Writer w "] ")
+  (print-dup (meta v) w)
+  (.write ^Writer w ")"))
--- test/ropes/core_test.clj
+++ test/ropes/core_test.clj
@@ -1,4 +1,71 @@
 (ns ropes.core-test
   (:require
+   [clojure.edn :as edn]
    [clojure.test :as t]
-   [ropes.core :as sut]))
+   [ropes.core :as sut])
+  (:import
+   (java.util ArrayList)))
+
+(t/deftest collections-implementation
+  (t/testing "ropes are counted"
+    (t/is (counted? (sut/rope)) "a rope satisfies the counted? predicate")
+    (t/is (zero? (count (sut/rope))) "an empty rope has a count of zero")
+    (t/is (= 1 (count (sut/rope [1]))) "a rope with one element has a count of one")
+    (t/is (= 1 (count (sut/snip (sut/rope (range 5)) 1))) "a snipped rope keeps its count")
+    (t/is (= 2 (count (sut/concat [1] [2]))) "concatenated ropes add their count"))
+  (t/testing "ropes have collection equality"
+    (t/is (= [1 2 3] (sut/rope [1 2 3])) "single node ropes are equal to collections")
+    (t/is (= [1 2 3] (conj (sut/rope [1 2]) 3)) "multi-node ropes are equal to collections")
+    (t/is (= (seq "abc") (sut/rope "abc")) "ropes from strings are equal to sequences from strings")
+    (t/is (= (doto (ArrayList.)
+               (.add 1)
+               (.add 2)
+               (.add 3))
+             (sut/rope [1 2 3]))
+          "ropes are equal to java.util.Lists"))
+  (t/testing "ropes can have metadata"
+    (t/is (= {:a 1} (meta (with-meta (sut/rope) {:a 1}))) "meta fetched from a rope it's stored on is the same"))
+  (t/testing "ropes conform to the hash spec"
+    (t/is (= (hash (sut/rope [1 2 3])) (hash [1 2 3]))
+          "a rope's hash is the same as a vector's with the same elements")))
+
+(t/deftest construction
+  (t/testing "empty ropes"
+    (t/is (= [] (sut/rope)) "empty ropes are equal to empty collections")
+    (t/is (= [] (empty (sut/rope [1 2 3]))) "empty ropes constructed with empty are equal to empty collections"))
+  (t/testing "ropes with elements"
+    (t/is (pos? (count (sut/rope [1 2 3]))) "ropes can be constructed with multiple elements"))
+  (t/testing "ropes are ropes"
+    (t/is (sut/rope? (sut/rope)) "empty ropes are ropes")
+    (t/is (sut/rope? (sut/rope [1 2 3])) "ropes with elements are ropes")))
+
+(t/deftest manipulation
+  (t/testing "concatenation"
+    (t/is (= [1] (sut/concat [] [1])) "an empty rope concatenated with one with elements has elements")
+    (t/is (= [1 2] (sut/concat [1] [2])) "two ropes concatenated together have both their elements")
+    (t/is (= [1 2 3] (sut/concat [1] [2] [3])) "three ropes concatenated together have all their elements"))
+  (t/testing "snipping"
+    (t/is (= [1 3] (sut/snip [1 2 3] 1 2)) "elements are snipped from the middle")
+    (t/is (= [1] (sut/snip [1 2 3] 1)) "elements are snipped till the end")
+    (t/is (= [] (sut/snip [1 2 3] 0)) "all elements can be snipped"))
+  (t/testing "insertion"
+    (t/is (= [1 :blah :blah2 2 3] (sut/insert [1 2 3] 1 [:blah :blah2]))
+          "insert adds elements in the middle of ropes")
+    (t/is (= [:blah 1 2 3] (sut/insert [1 2 3] 0 [:blah]))
+          "insert adds elements to the beginning of ropes"))
+  (t/testing "replacement"
+    (t/is (= [1 :blah :blah2 3] (sut/replace [1 2 3] 1 2 [:blah :blah2]))
+          "replaces elements from the middle of a rope")
+    (t/is (= [1 :blah :blah2 2 3] (sut/replace [1 2 3] 1 1 [:blah :blah2]))
+          "acts as insert with equal start and end")))
+
+(t/deftest serialization
+  (t/testing "print-dup"
+    (t/is (let [r (sut/rope [1 2 3])]
+            (= r (read-string (binding [*print-dup* true]
+                                (pr-str r)))))
+          "read ropes are equal to constructed ones"))
+  (t/testing "print-method"
+    (t/is (let [r (sut/rope [1 2 3])]
+            (= r (edn/read-string {:readers {'rope sut/rope}} (pr-str r))))
+          "read ropes are equal to constructed ones")))
